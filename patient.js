73ed47be92bf3f169a8d50435eaf7364e9ff0eb9
dhivyaa murugesh <141308298+Dhivyaa12@users.noreply.github.com>
2025-06-30 11:40:49 +0530

    Delete patient.js

--- patient.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://9dkceq681h.execute-api.ap-southeast-2.amazonaws.com/prod';
-
-export const getPatientInfo = async (patientId) => {
-  const response = await axios.get(`${BASE_URL}/patients/${patientId}`);
-  return response.data;
-};
